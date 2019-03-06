93e82e7a02b1fa48d9b9d0c7247b2b9ae6b5e5be
Амир Сулейменов <@corp.itstep>
2019-03-06 18:53:48 +0600

    XMLJsonHW

---
+++ XMLJsonHW/XMLJsonHW/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLJsonHW
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public Car()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ XMLJsonHW/XMLJsonHW/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+    namespace XMLJsonHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Mark ="Lada",
+                Model="Prius",
+                Price=5000
+            };
+            XmlDocument xmlDoc = new XmlDocument();
+            var rootElement = xmlDoc.CreateElement("car");
+            var idElement = xmlDoc.CreateElement("Id");
+            idElement.SetAttribute("type", "Guid");
+            idElement.InnerText = car.Id.ToString();
+            rootElement.AppendChild(idElement);
+            var markElement = xmlDoc.CreateElement("Mark");
+            markElement.InnerText = car.Mark;
+            rootElement.AppendChild(markElement);
+            var modelElement = xmlDoc.CreateElement("Model");
+            modelElement.InnerText = car.Model;
+            rootElement.AppendChild(modelElement);
+            var priceElement = xmlDoc.CreateElement("Price");
+            priceElement.InnerText = car.Price.ToString();
+            rootElement.AppendChild(priceElement);
+            xmlDoc.AppendChild(rootElement);
+            xmlDoc.Save("cars.xml");
+            
+        }
+    }
+}
