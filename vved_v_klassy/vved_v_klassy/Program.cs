d07128e1451872dfbfb8b4cc32e044b12a86da35
Амир Сулейменов <amirdragon0808@gmail.com>
2019-04-04 16:51:40 +0600

    peresdat

---
+++ vved_v_klassy/vved_v_klassy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vved_v_klassy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Article ar = new Article();
+            string startMark = "Ferrari";
+            Car[] cars = new Car[5];
+            cars[0] = new Car(4, "toyota", 15, 4, "Oil", 2.5, 1.3);
+            cars[1] = new Car(4, startMark, 15, 4, "Oil", 2.5, 1.3);
+            cars[2] = new Car(4, "lamborghini", 25, 4, "Oil", 8.5, 1.3);
+            cars[3] = new Car(4, startMark, 15, 4, "Oil", 2.5, 1.3);
+            cars[4] = new Car(4, "toyota", 15, 4, "Oil", 2.5, 1.3);
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i].InfoMark();
+            }
+
+        }
+    }
+    public partial class Car
+    {
+        public void InfoMark()
+        {
+            Console.WriteLine(_mark);
+        }
+    }
+}
---
+++ vved_v_klassy/vved_v_klassy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("vved_v_klassy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vved_v_klassy")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2059e74a-f475-47e5-bac7-7365ee1825eb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
