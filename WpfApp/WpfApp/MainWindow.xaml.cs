a2194e4bec33973e9850a7cdedce209cf5fec39e
Амир Сулейменов <amirdragon0808@gmail.com>
2019-04-29 10:30:03 +0600

    WpfStart

---
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Student student = new Student
+            {
+                Name = "Vasya",
+                Surname = "Pupkin",
+                DateBirth = DateTime.Now.AddYears(-22),
+                AverageMark=5.5,
+                Course=2
+            };
+            InitializeComponent();
+            Student.Text = $" Name: {student.Name}, \n Surname: {student.Surname}, \n Date of Birth: {student.DateBirth}, \n Average Mark: {student.AverageMark}, \n Course: {student.Course}";
+        }
+    }
+}
---
+++ WpfApp/WpfApp/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateBirth { get; set; }
+        public double AverageMark { get; set; }
+        public int Course { get; set; }
+    }
+}
